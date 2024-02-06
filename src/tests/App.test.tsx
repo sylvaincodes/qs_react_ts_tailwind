c02fd204170a37c40b4eacf3a16b4d6c925ece4a
sylvaincodes <sylvaincodeur@gmail.com>
2024-02-06 18:00:14 +0100

    test: configuration des outils pour le testing

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/jest-globals";
+import App from "../App";
+
+describe("", () => {
+  //first test
+  it("should render without crash", () => {
+    //Arrange
+    const { container } = render(<App />);
+
+    //Assertions
+    expect(container).toBeDefined();
+  });
+});
