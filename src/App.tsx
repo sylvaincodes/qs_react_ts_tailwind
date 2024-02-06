c2196efe7ac5e790b0346e920fba5cb5716c8c4d
sylvaincodes <sylvaincodeur@gmail.com>
2024-02-06 14:07:21 +0100

    npm: initialisation du projet creation des fichiers et dossiers de base  - installation des dependances react react-dom typescript env-cmd et vite - configuration des scripts et creation du config typescript  -  Composant: creation contenu App et affichage dans dom - lint: configuration de eslint avec npm i eslint & npx eslint --init && npm i esling-config-airbnb

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+function App (props: IAppProps) {
+  return (
+    <div>
+    <h1>qds</h1>
+    </div>
+    );
+  }
+  
+  export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import ReactDOM from "react-dom/client"
+import  App  from "./App";
+
+const node = document.querySelector('#root');
+ReactDOM.createRoot( node).render(<App/>);
+
